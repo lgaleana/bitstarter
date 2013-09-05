85d870e35ff993e0f606a92f9d396c62a81a5807
Luis Galeana <lsgaleana@gmail.com>
2013-09-05 04:59:37 +0000

    Added files to save emails to database.

--- web.js
+++ web.js
@@ -1,13 +1,66 @@
-var express = require('express');
+// Define routes for simple SSJS web app. 
+var async   = require('async')
+  , express = require('express')
+  , fs      = require('fs')
+  , http    = require('http')
+  , https   = require('https')
+  , db      = require('./models');
+
 var app = express();
-app.use(express.logger());
-var fs = require('fs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 8080);
 
+// Render homepage (note trailing slash): example.com/
 app.get('/', function(request, response) {
-  response.send(new Buffer(fs.readFileSync("index.html")).toString());
+  var data = fs.readFileSync('index.html').toString();
+  response.send(data);
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// Render invites
+/*app.get('/invites', function(request, response) {
+  global.db.Invite.findAll().success(function(invites) {
+    var invites_json = [];
+    invites.forEach(function(invite) {
+      invites_json.push({email: order.email, time: invite.time});
+    });
+    // Uses views/invites.ejs
+    response.render("invites", {invites: invites_json});
+  }).error(function(err) {
+    console.log(err);
+    response.send("error retrieving orders");
+  });
+});*/
+
+// sync the database and start the server
+db.sequelize.sync().complete(function(err) {
+  if (err) {
+    throw err;
+  } else {
+    http.createServer(app).listen(app.get('port'), function() {
+      console.log("Listening on " + app.get('port'));
+    });
+  }
 });
+
+// add order to the database if it doesn't already exist
+app.post('/invites', function(request, response) {
+  var email = request.body.email;
+  var time = new Date().getTime();
+  var Invite = global.db.Invite;
+  // find if invite has already been added
+  Invite.find({where: {email: request.body.email.email}}).success(function(invite_instance) {
+    if (!invite_instance) {
+      // build instance if doesn't exist and save
+      var new_invite_instance = Invite.build({
+        email: email,
+        time: time
+      });
+      new_invite_instance.save().success(function() {
+        console.log('New invite requested. Email: ' + email);
+      }).error(function(err) {
+        console.log(err);
+      });
+    }
+  });
+}
